b6b80d3aff0cecb4c5bcf335081c043166b0f85c
Arakyl1 <62062105+Arakyl1@users.noreply.github.com>
2020-10-28 13:51:58 +0300

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,36 @@
+let carts = $(".proguct-cart");
+let col = $(".col");
+let buttonSey = $(".additional-info a");
+
+$(carts).click(function () { 
+  if ($(this).parents().hasClass("not-on-sale")) {
+  } else  if ($(this).parents().hasClass("active")) {
+    $(this).parents().toggleClass("active");
+    $(this).toggleClass ("active");
+  } 
+  else {
+    $(this).parents().addClass("active");
+    $(this).addClass("active");
+  }
+});
+
+$(buttonSey).click(function () { 
+  $(this).parents(".col").addClass("active");
+  $(carts, this).addClass("active");
+});
+
+$(col).on('mouseover', function(){
+  if ($(this).hasClass("active")) {
+    if ($(this).children(carts).hasClass("active")) {
+      $(".business-name", this).replaceWith("<p class='business-name'>Сказочное заморское яство</p>");
+    }
+  }
+});
+$(col).on('mouseout', function(){
+  if ($(this).hasClass("active")) {
+    if ($(this).children(carts).hasClass("active")) {
+      $(".business-name", this).replaceWith("<p class='business-name' style=' color: #d91667;'>Котэ не одобряет?</p>");
+      
+    }
+  }
+});
