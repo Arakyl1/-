310e1f2bfb91d1d014af1b9835314ccfabf2498c
Arakyl1 <62062105+Arakyl1@users.noreply.github.com>
2020-11-20 20:57:15 +0300

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -2,22 +2,34 @@ let carts = $(".proguct-cart");
 let col = $(".col");
 let buttonSey = $(".additional-info a");
 
+
+
 $(carts).click(function () { 
   if ($(this).parents().hasClass("not-on-sale")) {
+    $(this).parents(".col").toggleClass("soldOut");
   } else  if ($(this).parents().hasClass("active")) {
-    $(this).parents().toggleClass("active");
-    $(carts).addClass ("active");
+    $(this).parents(".col").removeClass("active");
+    $(this).removeClass ("active");
   } 
   else {
-    $(this).parents().addClass("active");
+    $(this).parents(".col").addClass("active");
     $(this).addClass("active");
   }
 });
 
+
+
 $(buttonSey).click(function () { 
+  if ($(this).parents().hasClass("not-on-sale")) {
+    $(this).parents(".col").toggleClass("soldOut");
+  }
+else{
   $(this).parents(".col").addClass("active");
   $(carts).addClass("active");
+}
 });
+
+
 
 $(col).on('mouseover', function(){
   if ($(this).hasClass("active")) {
